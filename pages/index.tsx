2863a3d78d60dc65e9a2f17d271d423465f43151
vorsakha <andreitf.dev@gmail.com>
2021-08-17 10:19:47 -0300

    home - filter object array by date

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,17 @@ import Jobs from "../components/Jobs";
 import Pagination from "../components/Pagination";
 import { Context } from "./_app";
 
+interface DateType {
+  body: string;
+  html_url: string;
+  created_at: string;
+  id: number;
+  labels: {
+    name: string;
+  }[];
+  title: string;
+}
+
 export async function getStaticProps() {
   try {
     const rbrRes = await fetch(
@@ -32,10 +43,21 @@ const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const context = useContext(Context);
 
   useEffect(() => {
-    context?.setJobs(data);
+    const staticData: DateType[] = data;
+
+    const filterJobs = staticData.sort((a, b) => {
+      const dateA: any = new Date(a.created_at);
+      const dateB: any = new Date(b.created_at);
+
+      return dateB - dateA;
+    });
+
+    context?.setJobs(filterJobs);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  console.log(context?.jobs);
 
   return (
     <div>
@@ -48,9 +70,9 @@ const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
       <main className="font-mono min-h-total max-w-screen-lg m-auto">
         <Header />
 
-        <Jobs />
-
         <Pagination />
+
+        <Jobs />
       </main>
 
       <Footer />
