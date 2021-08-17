75d62a1cbd406e0b9fad3a76d1cf076716fb98fe
vorsakha <andreitf.dev@gmail.com>
2021-08-17 14:09:24 -0300

    Details styles

--- components/Details/index.tsx
+++ components/Details/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
@@ -16,10 +17,19 @@ interface DataType {
 
 const Details = ({ data }: DataType) => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">{data.title}</h1>
+    <div className="text-gray-800">
+      <Link href="/">
+        <a
+          className={`px-4 py-1 rounded shadow-md text-gray-100 hover:text-gray-100 bg-purple-700 hover:bg-purple-900`}
+        >
+          {"Voltar"}
+        </a>
+      </Link>
+      <h1 className="mt-4 text-4xl font-bold">{data.title}</h1>
       <div className="font-nunito my-4">
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{data.body}</ReactMarkdown>
+        <ReactMarkdown skipHtml={true} remarkPlugins={[remarkGfm]}>
+          {data.body}
+        </ReactMarkdown>
       </div>
     </div>
   );
