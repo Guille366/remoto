90e118a2406ce19d3188dc9e966c262919244aa6
vorsakha <andreitf.dev@gmail.com>
2021-08-17 16:01:20 -0300

    footer - add credits

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,20 @@
 const Footer: React.FC = () => {
   return (
     <footer className="flex items-center justify-center w-full h-14 font-mono">
-      <div className="w-full flex flex-row justify-center items-center m-auto h-14 max-w-screen-lg border-t border-gray-200">
+      <div className="w-full flex flex-row justify-between items-center m-auto h-14 max-w-screen-lg border-t border-gray-200">
         <p className="text-center text-gray-400 text-xs">
-          &copy;2021 <span className="font-blops text-gray-400">REMOTO</span>.
+          &copy;2021 <span className="font-bold text-gray-400">REMOTO</span>.
           Todos os direitos reservados.
+        </p>
+        <p className="text-center text-gray-400 text-xs">
+          by{" "}
+          <a
+            href="https://github.com/vorsakha"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @vorsakha
+          </a>
         </p>
       </div>
     </footer>
