4d7970df9dde9a08baf14086f654d22d3a57ad88
vorsakha <andreitf.dev@gmail.com>
2021-08-17 14:06:20 -0300

    Header color

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <div className="w-full text-center py-8">
       <Link href="/">
-        <a className="font-mono text-7xl font-bold ">REMOTO</a>
+        <a className="font-mono text-7xl font-bold text-gray-800 hover:text-gray-800">
+          REMOTO
+        </a>
       </Link>
     </div>
   );
