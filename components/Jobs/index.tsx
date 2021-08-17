609d0e349322ad20d7a87283212b3083d100fd7b
vorsakha <andreitf.dev@gmail.com>
2021-08-17 10:24:04 -0300

    jobs - arr fallback on jsx / formatDate implementation

--- components/Jobs/index.tsx
+++ components/Jobs/index.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { Context } from "../../pages/_app";
+import formatDate from "../../utils/formatDate";
 
 interface StateTypes {
   body: string;
@@ -31,17 +32,24 @@ const Jobs = () => {
     setArr(limited || []);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [context?.n]);
+  }, [context?.n, context?.jobs]);
 
   return (
     <div className="font-nunito">
       <ul>
-        {arr?.map((item) => (
-          <li key={item.id}>
-            {item.title}
-            <br /> {item.labels.map((item) => item.name)}
+        {arr.length === 0 ? (
+          <li>
+            <h2 className="w-full text-center pt-12 text-xl">No items...</h2>
           </li>
-        ))}
+        ) : (
+          arr.map((item) => (
+            <li key={item.id}>
+              {item.title}
+              <br /> {item.labels.map((item) => item.name)}
+              <br /> {formatDate(item.created_at)}
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
