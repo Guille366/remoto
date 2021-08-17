2f0e1fe5b1f9ce9ca0093a3785a159cadf6d988f
vorsakha <andreitf.dev@gmail.com>
2021-08-17 08:44:41 -0300

    add context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/global.css";
+import type { AppProps } from "next/app";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
+
+interface JobsTypes {
+  body: string;
+  html_url: string;
+  created_at: string;
+  id: number;
+  labels: {
+    name: string;
+  }[];
+  title: string;
+}
+interface ContextTypes {
+  n: number;
+  setN: Dispatch<SetStateAction<number>>;
+  pagination: number[] | null;
+  setPagination: Dispatch<SetStateAction<number[] | null>>;
+  jobs: JobsTypes[];
+  setJobs: Dispatch<SetStateAction<never[]>>;
+}
+
+export const Context = createContext<ContextTypes | null>(null);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [n, setN] = useState(10);
+  const [pagination, setPagination] = useState<number[] | null>(null);
+  const [jobs, setJobs] = useState([]);
+
+  return (
+    <Context.Provider
+      value={{ n, setN, pagination, setPagination, setJobs, jobs }}
+    >
+      <Component {...pageProps} />
+    </Context.Provider>
+  );
 }
-export default MyApp
+export default MyApp;
