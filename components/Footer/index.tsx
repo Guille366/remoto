51e1f3805ec923443e3447ed88d3edf9866c1f1e
vorsakha <andreitf.dev@gmail.com>
2021-08-17 16:07:41 -0300

    footer - fix spacing

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,12 +1,12 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="flex items-center justify-center w-full h-14 font-mono">
+    <footer className="flex items-center justify-center w-full h-14 font-mono px-4">
       <div className="w-full flex flex-row justify-between items-center m-auto h-14 max-w-screen-lg border-t border-gray-200">
-        <p className="text-center text-gray-400 text-xs">
+        <p className="text-start text-gray-400 text-xs">
           &copy;2021 <span className="font-bold text-gray-400">REMOTO</span>.
           Todos os direitos reservados.
         </p>
-        <p className="text-center text-gray-400 text-xs">
+        <p className="text-end text-gray-400 text-xs">
           by{" "}
           <a
             href="https://github.com/vorsakha"
