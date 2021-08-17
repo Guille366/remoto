fd70496658a71d3e66d98c383036f8ced530cf3c
vorsakha <andreitf.dev@gmail.com>
2021-08-17 08:46:15 -0300

    tailwind + nunito font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
