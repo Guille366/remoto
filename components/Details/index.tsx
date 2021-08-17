19ea0cf429e2a9f70d1667af491ff5a4c729b9ee
vorsakha <andreitf.dev@gmail.com>
2021-08-17 16:35:16 -0300

    Details - link styling

--- components/Details/index.tsx
+++ components/Details/index.tsx
@@ -19,11 +19,7 @@ const Details = ({ data }: DataType) => {
   return (
     <div className="text-gray-800">
       <Link href="/">
-        <a
-          className={`px-4 py-1 rounded shadow-md text-gray-100 hover:text-gray-100 bg-purple-700 hover:bg-purple-900`}
-        >
-          {"Voltar"}
-        </a>
+        <a className={`py-1`}>{"<< Voltar"}</a>
       </Link>
       <h1 className="mt-4 text-4xl font-bold">{data.title}</h1>
       <div className="font-nunito my-4">
