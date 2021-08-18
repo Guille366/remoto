3271b286e340807345b61e0a796c003fafbf2840
vorsakha <andreitf.dev@gmail.com>
2021-08-18 10:53:19 -0300

    Header - add real descrip / no-underline

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,14 +5,14 @@ const Header = () => {
   return (
     <div className="w-full text-center py-8">
       <Link href="/">
-        <a className="font-mono text-7xl font-bold text-gray-800 hover:text-gray-800">
+        <a className="font-mono text-6xl md:text-7xl no-underline font-bold text-gray-800 hover:text-gray-800">
           <span className="text-purple-700">{"<"}</span>
           {"REMOTO"}
           <span className="text-purple-700">{"/>"}</span>
         </a>
       </Link>
       <p className="text-gray-500 text-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, qui.
+        Dev jobs para trabalhar de onde quiser.
       </p>
     </div>
   );
