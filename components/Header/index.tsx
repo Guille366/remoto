52ce3f37bc5084df365776cfa2f852f977ee1cba
vorsakha <andreitf.dev@gmail.com>
2021-08-18 08:33:58 -0300

    Header - increment logo / memo / description

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
+import { memo } from "react";
 
 const Header = () => {
   return (
     <div className="w-full text-center py-8">
       <Link href="/">
         <a className="font-mono text-7xl font-bold text-gray-800 hover:text-gray-800">
-          REMOTO
+          <span className="text-purple-700">{"<"}</span>
+          {"REMOTO"}
+          <span className="text-purple-700">{"/>"}</span>
         </a>
       </Link>
+      <p className="text-gray-500 text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, qui.
+      </p>
     </div>
   );
 };
 
-export default Header;
+export default memo(Header);
