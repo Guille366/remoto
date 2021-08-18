5d152775e8d03b4f2f5758d8dad33f1b1dcc0358
vorsakha <andreitf.dev@gmail.com>
2021-08-18 10:50:42 -0300

    details - add padding-y / edit back btn

--- components/Details/index.tsx
+++ components/Details/index.tsx
@@ -17,9 +17,9 @@ interface DataType {
 
 const Details = ({ data }: DataType) => {
   return (
-    <div className="text-gray-800">
+    <div className="text-gray-800 py-4">
       <Link href="/">
-        <a className={`py-1`}>{"<< Voltar"}</a>
+        <a className={`py-1`}>{"< Voltar"}</a>
       </Link>
       <h1 className="mt-4 text-3xl md:text-4xl font-bold">{data?.title}</h1>
       <div className="font-nunito my-4">
