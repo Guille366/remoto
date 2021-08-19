ad6bdf95e5c8381133173a42ea526f5783348d4f
vorsakha <andreitf.dev@gmail.com>
2021-08-19 09:47:39 -0300

    add pwa manifest plugin

---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+const APP_NAME = "REMOTO";
+const APP_DESCRIPTION = "Dev jobs para trabalhar de onde quiser.";
+
+export default class extends Document {
+  static async getInitialProps(ctx) {
+    return await Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br" dir="ltr">
+        <Head>
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#6D28D9" />
+          {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+          {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
+
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
