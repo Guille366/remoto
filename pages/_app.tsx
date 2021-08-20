62aff24de63706732ece9367fe93f2c0d76e3b67
vorsakha <andreitf.dev@gmail.com>
2021-08-20 10:55:33 -0300

    _app - fav contexts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,24 +24,43 @@ interface ContextTypes {
   setJobs: Dispatch<SetStateAction<JobsTypes[] | []>>;
   setPage: Dispatch<SetStateAction<number>>;
   page: number;
+  setFavPage: Dispatch<SetStateAction<number>>;
+  favPage: number;
   alert: {
     msg: string | null;
     active: boolean;
   };
   setAlert: Dispatch<SetStateAction<AlertTypes>>;
+  favN: number;
+  setFavN: Dispatch<SetStateAction<number>>;
 }
 
 export const Context = createContext<ContextTypes | null>(null);
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [n, setN] = useState<number>(10);
+  const [favN, setFavN] = useState<number>(10);
   const [jobs, setJobs] = useState<JobsTypes[] | []>([]);
   const [page, setPage] = useState<number>(1);
+  const [favPage, setFavPage] = useState<number>(1);
   const [alert, setAlert] = useState<AlertTypes>({ msg: null, active: false });
 
   return (
     <Context.Provider
-      value={{ n, setN, setJobs, jobs, page, setPage, alert, setAlert }}
+      value={{
+        n,
+        setN,
+        setJobs,
+        jobs,
+        page,
+        setPage,
+        alert,
+        setAlert,
+        favN,
+        setFavN,
+        favPage,
+        setFavPage,
+      }}
     >
       <Component {...pageProps} />
     </Context.Provider>
