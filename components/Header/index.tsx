6b1f0edd42c52a719198574183a35cf6db9bb56c
vorsakha <andreitf.dev@gmail.com>
2021-08-20 10:56:19 -0300

    Header - Favs btn

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { memo } from "react";
 
-const Header = () => {
+const Header = ({ fav }: { fav?: boolean }) => {
   return (
     <div className="w-full text-center py-8">
       <Link href="/">
@@ -14,6 +14,15 @@ const Header = () => {
       <p className="text-gray-500 text-sm">
         Dev jobs para trabalhar de onde quiser.
       </p>
+      <div className={`${fav ? "hidden" : "block"} m-auto pt-4`}>
+        <Link href="/favs">
+          <a
+            className={`font-nunito font-bold no-underline px-4 py-2 rounded shadow-md text-gray-100 bg-purple-700 hover:bg-purple-900 hover:shadow-lg`}
+          >
+            Favoritos
+          </a>
+        </Link>
+      </div>
     </div>
   );
 };
