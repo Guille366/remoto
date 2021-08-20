d6c1d7eedaf739fe3976ecbfc7f46e58fd2ff832
vorsakha <andreitf.dev@gmail.com>
2021-08-20 10:57:25 -0300

    Pagination - refactored to handle more pages

--- components/Pagination/index.tsx
+++ components/Pagination/index.tsx
@@ -1,94 +1,165 @@
 import { useContext, useEffect, useState } from "react";
 import { Context } from "../../pages/_app";
+import range from "../../utils/range";
 
-const Pagination = () => {
+const Pagination = ({
+  fav,
+  favLength = 0,
+}: {
+  fav?: boolean;
+  favLength?: number;
+}) => {
   const [pagination, setPagination] = useState<number[]>([]);
+  const [limitedPag, setLimitedPag] = useState<number[]>([]);
+  const [limit, setLimit] = useState<number[]>([0, 5]);
 
   const context = useContext(Context);
 
   useEffect(() => {
-    const length = context?.jobs.length || 0;
-    const pages = length / 10;
-    const paginationArr = [];
+    const length = fav ? favLength : context?.jobs.length || 0;
+    const pages = Math.ceil(length / 10);
 
-    for (let i = 1; i <= pages; i++) {
-      paginationArr.push(i);
-    }
-
-    setPagination(paginationArr);
-  }, [context?.jobs]);
+    setPagination(range(1, pages));
+  }, [fav, favLength, context?.jobs.length]);
 
   useEffect(() => {
-    const p = context?.n || 0;
+    const filter = pagination.slice(limit[0], limit[1]);
+
+    setLimitedPag(filter);
+  }, [limit, pagination, context?.page]);
+
+  useEffect(() => {
+    const p = fav ? context?.favN || 0 : context?.n || 0;
     const result = p / 10;
 
-    context?.setPage(result);
-  }, [pagination, context?.n, context]);
+    fav ? context?.setFavPage(result) : context?.setPage(result);
+  }, [pagination, context?.n, context, fav]);
+
+  useEffect(() => {
+    const page = context?.page || 1;
+
+    if (page >= 5) {
+      if (pagination.length - 5 < page) {
+        return setLimit([pagination.length - 5, pagination.length]);
+      }
+      setLimit([limit[0] + 1, page + 1]);
+    }
+    if (page < 5) {
+      if (pagination.length <= 5) {
+        setLimit([0, pagination.length]);
+        return;
+      }
+
+      setLimit([0, 5]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [context?.page, pagination.length]);
 
   function handleBtn(item: number) {
-    context?.setN(10 * item);
+    const page = context?.page || 1;
+    if (fav) {
+      context?.setFavN(10 * item);
 
-    window.scrollTo(0, 0);
-  }
+      if (page >= 5) {
+        context?.favPage === limit[1] &&
+          setLimit([limit[0] + 1, context?.favPage + 1]);
+      }
+      if (page < 5) {
+        if (pagination.length <= 5) {
+          setLimit([0, pagination.length]);
 
-  function handleBtnMobile(direction: string) {
-    switch (direction) {
-      case "previous":
-        context?.setN(context?.n - 10);
-        break;
-      case "next":
-        context?.setN(context?.n + 10);
-        break;
-      default:
-        break;
+          window.scrollTo(0, 0);
+          return;
+        }
+
+        setLimit([0, 5]);
+      }
+    } else {
+      context?.setN(10 * item);
+
+      if (page >= 5) {
+        if (pagination.length - 5 < page) {
+          setLimit([pagination.length - 5, pagination.length]);
+          window.scrollTo(0, 0);
+
+          return;
+        }
+        setLimit([limit[0] + 1, page + 1]);
+      }
+      if (page < 5) {
+        if (pagination.length <= 5) {
+          setLimit([0, pagination.length]);
+          window.scrollTo(0, 0);
+
+          return;
+        }
+
+        setLimit([0, 5]);
+      }
     }
 
     window.scrollTo(0, 0);
+    return;
   }
+
+  console.log(context?.page);
 
   return (
     <>
-      <div className="hidden md:flex flex-row justify-center my-8 font-nunito">
-        {pagination.map((item) => (
-          <div className="mx-2" key={item}>
-            <button
-              className={`${
-                context?.n === item * 10 ? "bg-purple-900" : "bg-purple-700"
-              } px-4 py-1 rounded shadow-md text-gray-100 hover:bg-purple-900 hover:shadow-lg`}
-              onClick={() => handleBtn(item)}
-            >
-              {item}
-            </button>
+      {pagination.length > 1 && (
+        <>
+          <div className="flex flex-row justify-center my-8 font-nunito">
+            {limit[1] > 5 && (
+              <>
+                <button
+                  className="px-4 py-1 font-bold rounded shadow-md text-gray-100 bg-purple-700 hover:bg-purple-900 hover:shadow-lg"
+                  onClick={() => handleBtn(1)}
+                >
+                  {1}
+                </button>
+                <span className="px-2 py-1 font-bold">...</span>
+              </>
+            )}
+            {limitedPag.map((item) => (
+              <div className="mx-1" key={item}>
+                <button
+                  className={`${
+                    !fav && context?.n === item * 10
+                      ? "bg-purple-900"
+                      : "bg-purple-700"
+                  } ${
+                    fav && context?.favN === item * 10
+                      ? "bg-purple-900"
+                      : "bg-purple-700"
+                  } px-4 py-1 font-bold rounded shadow-md text-gray-100 hover:bg-purple-900 hover:shadow-lg`}
+                  onClick={() => handleBtn(item)}
+                >
+                  {item}
+                </button>
+              </div>
+            ))}
+            {pagination.length > limit[1] && (
+              <>
+                <span className="px-2 py-1 font-bold">...</span>
+                <button
+                  className={`${
+                    !fav && context?.n === 9 * 10
+                      ? "bg-purple-900"
+                      : "bg-purple-700"
+                  } ${
+                    fav && context?.favN === 9 * 10
+                      ? "bg-purple-900"
+                      : "bg-purple-700"
+                  } px-4 py-1 font-bold rounded shadow-md text-gray-100 bg-purple-700 hover:bg-purple-900 hover:shadow-lg`}
+                  onClick={() => handleBtn(pagination.length)}
+                >
+                  {pagination.length}
+                </button>
+              </>
+            )}
           </div>
-        ))}
-      </div>
-
-      <div className="md:hidden block">
-        <p className="w-full text-center text-sm"></p>
-        <div className="md:hidden flex flex-row justify-center mb-8">
-          <div className="mx-2">
-            <button
-              className={`px-4 py-1 rounded shadow-md text-gray-100 bg-purple-700 hover:bg-purple-900 hover:shadow-lg`}
-              onClick={() => handleBtnMobile("previous")}
-              disabled={context?.n === 10}
-            >
-              {"<"}
-            </button>
-          </div>
-          <div className="mx-2 flex align-center justify-center">
-            {context?.page}/{pagination.length}
-          </div>
-          <div className="mx-2">
-            <button
-              className={`px-4 py-1 rounded shadow-md text-gray-100 bg-purple-700 hover:bg-purple-900 hover:shadow-lg`}
-              onClick={() => handleBtnMobile("next")}
-              disabled={context?.n === 100}
-            >
-              {">"}
-            </button>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 };
