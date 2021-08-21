6d0aa2f9d854142454c090e2b8d1244eb7b70a56
vorsakha <andreitf.dev@gmail.com>
2021-08-21 15:23:30 -0300

    Pagination - refactored

--- components/Pagination/index.tsx
+++ components/Pagination/index.tsx
@@ -1,122 +1,99 @@
-import { useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { Context } from "../../pages/_app";
 import range from "../../utils/range";
 
-const Pagination = ({
-  fav,
-  favLength = 0,
-}: {
-  fav?: boolean;
-  favLength?: number;
-}) => {
+const Pagination = () => {
   const [pagination, setPagination] = useState<number[]>([]);
   const [limitedPag, setLimitedPag] = useState<number[]>([]);
-  const [limit, setLimit] = useState<number[]>([0, 5]);
+  const [limit, setLimit] = useState<number[]>([0, 3]);
 
   const context = useContext(Context);
 
+  // Update pagination array when jobs context is changed
   useEffect(() => {
-    const length = fav ? favLength : context?.jobs.length || 0;
+    const length = context?.jobs.length || 0;
     const pages = Math.ceil(length / 10);
 
     setPagination(range(1, pages));
-  }, [fav, favLength, context?.jobs.length]);
+  }, [context?.jobs.length]);
 
+  // Update limited pagination array every time limit is changed
   useEffect(() => {
-    const filter = pagination.slice(limit[0], limit[1]);
+    const indexOne = limit[0] > 0 ? limit[0] : 0;
+    const indexTwo = limit[1] < 3 ? 3 : limit[1];
+    const filter =
+      limit[1] - limit[0] === 3 && pagination.slice(indexOne, indexTwo);
 
-    setLimitedPag(filter);
-  }, [limit, pagination, context?.page]);
+    filter !== false && setLimitedPag(filter);
+  }, [limit, pagination]);
 
+  // Update page to context every pagination array update
   useEffect(() => {
-    const p = fav ? context?.favN || 0 : context?.n || 0;
+    const p = context?.n || 0;
     const result = p / 10;
 
-    fav ? context?.setFavPage(result) : context?.setPage(result);
-  }, [pagination, context?.n, context, fav]);
+    context?.setPage(result);
+  }, [context, pagination]);
 
+  // onClick pagination fnc with useCallback so it don't update unnecessarily
+  const handlePagination = useCallback(
+    (item) => {
+      const page = context?.page || 1;
+
+      context?.setN(10 * item);
+
+      if (page >= 3) {
+        if (pagination.length - 2 < page && pagination.length - 3 > 0) {
+          setLimit([pagination.length - 3, pagination.length]);
+          window.scrollTo(0, 0);
+
+          return;
+        }
+
+        if (page > item && item - 1 - 3 >= 1) {
+          setLimit([item - 3, item + 1]);
+        } else {
+          setLimit([item + 1 - 3, item + 1]);
+        }
+
+        window.scrollTo(0, 0);
+        return;
+      } else {
+        if (pagination.length <= 3) {
+          setLimit([0, pagination.length]);
+          window.scrollTo(0, 0);
+
+          return;
+        }
+
+        setLimit([0, 3]);
+      }
+
+      window.scrollTo(0, 0);
+      return;
+    },
+    [context, pagination]
+  );
+
+  // Effect to update state with the page transition, once btn clicked
   useEffect(() => {
     const page = context?.page || 1;
 
-    if (page >= 5) {
-      if (pagination.length - 5 < page) {
-        return setLimit([pagination.length - 5, pagination.length]);
-      }
-      setLimit((prev) => [prev[0] + 1, page + 1]);
-    }
-    if (page < 5) {
-      if (pagination.length <= 5) {
-        setLimit([0, pagination.length]);
-        return;
-      }
+    page > 2 && handlePagination(page);
 
-      setLimit([0, 5]);
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [context?.page, pagination.length]);
-
-  function handleBtn(item: number) {
-    const page = context?.page || 1;
-
-    if (fav) {
-      context?.setFavN(10 * item);
-
-      if (page >= 5) {
-        context?.favPage === limit[1] &&
-          setLimit([limit[0] + 1, context?.favPage + 1]);
-      }
-      if (page < 5) {
-        if (pagination.length <= 5) {
-          setLimit([0, pagination.length]);
-
-          window.scrollTo(0, 0);
-          return;
-        }
-
-        setLimit([0, 5]);
-      }
-    } else {
-      context?.setN(10 * item);
-
-      if (page >= 5) {
-        if (pagination.length - 5 < page) {
-          setLimit([pagination.length - 5, pagination.length]);
-          window.scrollTo(0, 0);
-
-          return;
-        }
-
-        // setLimit((prev) => [prev[0] + 1, page + 1]);
-
-        window.scrollTo(0, 0);
-
-        return;
-      } else {
-        if (pagination.length <= 5) {
-          setLimit([0, pagination.length]);
-          window.scrollTo(0, 0);
-
-          return;
-        }
-
-        setLimit([0, 5]);
-      }
-    }
-
-    window.scrollTo(0, 0);
-    return;
-  }
+  }, [context?.page]);
 
   return (
     <>
       {pagination.length > 1 && (
         <>
           <div className="flex flex-row justify-center my-8 font-nunito">
-            {limit[1] > 5 && (
+            {limit[1] > 3 && (
               <>
                 <button
                   className="px-4 py-1 font-bold rounded shadow-md text-gray-100 bg-purple-700 hover:bg-purple-900 hover:shadow-lg"
-                  onClick={() => handleBtn(1)}
+                  onClick={() => handlePagination(1)}
                 >
                   {1}
                 </button>
@@ -127,15 +104,9 @@ const Pagination = ({
               <div className="mx-1" key={item}>
                 <button
                   className={`${
-                    !fav && context?.n === item * 10
-                      ? "bg-purple-900"
-                      : "bg-purple-700"
-                  } ${
-                    fav && context?.favN === item * 10
-                      ? "bg-purple-900"
-                      : "bg-purple-700"
+                    context?.n === item * 10 ? "bg-purple-900" : "bg-purple-700"
                   } px-4 py-1 font-bold rounded shadow-md text-gray-100 hover:bg-purple-900 hover:shadow-lg`}
-                  onClick={() => handleBtn(item)}
+                  onClick={() => handlePagination(item)}
                 >
                   {item}
                 </button>
@@ -146,15 +117,9 @@ const Pagination = ({
                 <span className="px-2 py-1 font-bold">...</span>
                 <button
                   className={`${
-                    !fav && context?.n === 9 * 10
-                      ? "bg-purple-900"
-                      : "bg-purple-700"
-                  } ${
-                    fav && context?.favN === 9 * 10
-                      ? "bg-purple-900"
-                      : "bg-purple-700"
-                  } px-4 py-1 font-bold rounded shadow-md text-gray-100 bg-purple-700 hover:bg-purple-900 hover:shadow-lg`}
-                  onClick={() => handleBtn(pagination.length)}
+                    context?.n === 9 * 10 ? "bg-purple-900" : "bg-purple-700"
+                  }  px-4 py-1 font-bold rounded shadow-md text-gray-100 bg-purple-700 hover:bg-purple-900 hover:shadow-lg`}
+                  onClick={() => handlePagination(pagination.length)}
                 >
                   {pagination.length}
                 </button>
