e04f3d7a69828f3de29075b7b9d705e37c0f5802
vorsakha <andreitf.dev@gmail.com>
2021-08-30 17:14:52 -0300

    Filter - spacing

--- components/Filter/index.tsx
+++ components/Filter/index.tsx
@@ -53,7 +53,7 @@ const Filter = () => {
       <div
         className={`${
           open ? "flex" : "hidden"
-        } sm:w-full absolute sm:static top-64 right-4 bg-purple-700 sm:bg-transparent text-white sm:text-gray-800 rounded shadow sm:shadow-none z-50 p-4 sm:pr-0 sm:flex-row flex-col items-start justify-end`}
+        } sm:w-full absolute sm:static top-64 right-4 bg-purple-700 sm:bg-transparent text-white sm:text-gray-800 rounded shadow sm:shadow-none z-50 px-4 py-2 sm:pr-0 sm:flex-row flex-col items-start justify-end`}
       >
         <div
           className={`${
