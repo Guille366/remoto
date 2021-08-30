086c551b01c4ed03ed225a3278a5616d3a47bc46
vorsakha <andreitf.dev@gmail.com>
2021-08-30 18:41:13 -0300

    removing static fetch from pages

--- pages/favs.tsx
+++ pages/favs.tsx
@@ -19,78 +19,80 @@ interface DataTypes {
   title: string;
 }
 
-export async function getStaticProps() {
-  const ghData = await axios.get("https://remoto.vercel.app/api/data");
-  const data = ghData.data;
+// export async function getStaticProps() {
+//   const ghData = await axios.get("https://remoto.vercel.app/api/data");
+//   const data = ghData.data;
 
-  return {
-    props: {
-      data: data.data,
-    },
-    revalidate: 43300,
+//   return {
+//     props: {
+//       data: data.data,
+//     },
+//     revalidate: 43300,
+//   };
+// }
+
+const Favorites = () =>
+  //   {
+  //   data,
+  // }: InferGetStaticPropsType<typeof getStaticProps>
+  {
+    const [favJobs, setFavJobs] = useState<DataTypes[]>([]);
+
+    const context = useContext(Context);
+
+    // useEffect(() => {
+    //   const staticData: DataTypes[] = data;
+
+    //   const filterByDate = staticData.sort((a, b) => {
+    //     const dateA: any = new Date(a.created_at);
+    //     const dateB: any = new Date(b.created_at);
+
+    //     return dateB - dateA;
+    //   });
+
+    //   const filterByFav = filterByDate.filter((item) =>
+    //     localStorage.getItem(String(item.id))
+    //   );
+
+    //   setFavJobs(filterByFav);
+
+    //   // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
+
+    // useEffect(() => {
+    //   const staticData: DataTypes[] = data;
+
+    //   const filterByFav = staticData.filter((item) =>
+    //     localStorage.getItem(String(item.id))
+    //   );
+
+    //   setFavJobs(filterByFav);
+
+    //   // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [context?.alert.active === true]);
+
+    return (
+      <div>
+        <Head>
+          <title>Favoritos | REMOTO</title>
+          <meta
+            name="description"
+            content="Dev jobs para trabalhar de onde quiser."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className="font-code min-h-total max-w-screen-lg m-auto px-4 text-gray-800">
+          <Header fav />
+
+          <FavJobs favData={favJobs} />
+
+          {/* <Pagination fav favLength={favJobs.length} /> */}
+        </main>
+
+        <Footer />
+      </div>
+    );
   };
-}
-
-const Favorites = ({
-  data,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [favJobs, setFavJobs] = useState<DataTypes[]>([]);
-
-  const context = useContext(Context);
-
-  useEffect(() => {
-    const staticData: DataTypes[] = data;
-
-    const filterByDate = staticData.sort((a, b) => {
-      const dateA: any = new Date(a.created_at);
-      const dateB: any = new Date(b.created_at);
-
-      return dateB - dateA;
-    });
-
-    const filterByFav = filterByDate.filter((item) =>
-      localStorage.getItem(String(item.id))
-    );
-
-    setFavJobs(filterByFav);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    const staticData: DataTypes[] = data;
-
-    const filterByFav = staticData.filter((item) =>
-      localStorage.getItem(String(item.id))
-    );
-
-    setFavJobs(filterByFav);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [context?.alert.active === true]);
-
-  return (
-    <div>
-      <Head>
-        <title>Favoritos | REMOTO</title>
-        <meta
-          name="description"
-          content="Dev jobs para trabalhar de onde quiser."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="font-code min-h-total max-w-screen-lg m-auto px-4 text-gray-800">
-        <Header fav />
-
-        <FavJobs favData={favJobs} />
-
-        {/* <Pagination fav favLength={favJobs.length} /> */}
-      </main>
-
-      <Footer />
-    </div>
-  );
-};
 
 export default Favorites;
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,104 +19,106 @@ interface DateType {
   title: string;
 }
 
-export async function getStaticProps() {
-  const ghData = await axios.get("https://remoto.vercel.app/api/data");
-  const data = ghData.data;
+// export async function getStaticProps() {
+//   const ghData = await axios.get("https://remoto.vercel.app/api/data");
+//   const data = ghData.data;
 
-  return {
-    props: {
-      data: data.data,
-    },
-    revalidate: 43300,
+//   return {
+//     props: {
+//       data: data.data,
+//     },
+//     revalidate: 43300,
+//   };
+// }
+
+const Home = () =>
+  // { data }: InferGetStaticPropsType<typeof getStaticProps>
+  {
+    const context = useContext(Context);
+
+    // useEffect(() => {
+    //   const staticData: DateType[] = data;
+
+    //   const filterJobs = staticData.sort((a, b) => {
+    //     const dateA: any = new Date(a.created_at);
+    //     const dateB: any = new Date(b.created_at);
+
+    //     return dateB - dateA;
+    //   });
+
+    //   // Array of filter state keys
+    //   let filterArgsArray = [];
+    //   for (const property in context?.filterArgs) {
+    //     const args: any = context?.filterArgs;
+
+    //     if (args[property]) {
+    //       switch (property) {
+    //         case "junior":
+    //           filterArgsArray.push("JUNIOR");
+    //           break;
+    //         case "senior":
+    //           filterArgsArray.push("SENIOR");
+    //           break;
+    //         default:
+    //           filterArgsArray.push(property.toUpperCase());
+    //           break;
+    //       }
+    //     }
+    //   }
+    //   const mapped = filterArgsArray.map((item) => item.toUpperCase());
+
+    //   // Filter filterJobs acording to keys of state
+    //   const filter =
+    //     filterArgsArray.length !== 0
+    //       ? filterJobs.filter((item) => {
+    //           let mappedValues: string[] = [];
+    //           item.labels.forEach((i) => {
+    //             if (i.name.includes("Júnior")) {
+    //               mappedValues.push("JUNIOR");
+    //               return;
+    //             }
+    //             if (i.name.includes("Sênior")) {
+    //               mappedValues.push("SENIOR");
+    //               return;
+    //             }
+
+    //             mappedValues.push(i.name.toUpperCase());
+    //             return;
+    //           });
+
+    //           return mapped.every((j) => mappedValues.some((z) => z.includes(j)));
+    //         })
+    //       : filterJobs;
+
+    //   context?.setJobs(filter);
+
+    //   // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [context?.filterArgs]);
+
+    return (
+      <div>
+        <Head>
+          <title>
+            {context?.page === 1 ? "" : `Página ${context?.page} | `}REMOTO
+          </title>
+          <meta
+            name="description"
+            content="Dev jobs para trabalhar de onde quiser."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className="font-code min-h-total max-w-screen-lg m-auto px-4 text-gray-800">
+          <Header />
+
+          <Jobs />
+
+          <Pagination />
+        </main>
+
+        <Footer />
+      </div>
+    );
   };
-}
-
-const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const context = useContext(Context);
-
-  useEffect(() => {
-    const staticData: DateType[] = data;
-
-    const filterJobs = staticData.sort((a, b) => {
-      const dateA: any = new Date(a.created_at);
-      const dateB: any = new Date(b.created_at);
-
-      return dateB - dateA;
-    });
-
-    // Array of filter state keys
-    let filterArgsArray = [];
-    for (const property in context?.filterArgs) {
-      const args: any = context?.filterArgs;
-
-      if (args[property]) {
-        switch (property) {
-          case "junior":
-            filterArgsArray.push("JUNIOR");
-            break;
-          case "senior":
-            filterArgsArray.push("SENIOR");
-            break;
-          default:
-            filterArgsArray.push(property.toUpperCase());
-            break;
-        }
-      }
-    }
-    const mapped = filterArgsArray.map((item) => item.toUpperCase());
-
-    // Filter filterJobs acording to keys of state
-    const filter =
-      filterArgsArray.length !== 0
-        ? filterJobs.filter((item) => {
-            let mappedValues: string[] = [];
-            item.labels.forEach((i) => {
-              if (i.name.includes("Júnior")) {
-                mappedValues.push("JUNIOR");
-                return;
-              }
-              if (i.name.includes("Sênior")) {
-                mappedValues.push("SENIOR");
-                return;
-              }
-
-              mappedValues.push(i.name.toUpperCase());
-              return;
-            });
-
-            return mapped.every((j) => mappedValues.some((z) => z.includes(j)));
-          })
-        : filterJobs;
-
-    context?.setJobs(filter);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [context?.filterArgs]);
-
-  return (
-    <div>
-      <Head>
-        <title>
-          {context?.page === 1 ? "" : `Página ${context?.page} | `}REMOTO
-        </title>
-        <meta
-          name="description"
-          content="Dev jobs para trabalhar de onde quiser."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="font-code min-h-total max-w-screen-lg m-auto px-4 text-gray-800">
-        <Header />
-
-        <Jobs />
-
-        <Pagination />
-      </main>
-
-      <Footer />
-    </div>
-  );
-};
 
 export default Home;
