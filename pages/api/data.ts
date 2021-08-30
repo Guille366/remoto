a6fc0a5c2ef1bdd62df528ff2bc49d0065ae62a8
vorsakha <andreitf.dev@gmail.com>
2021-08-30 16:53:20 -0300

    api - swr

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -69,10 +69,10 @@ export default async function miscHandler(
           data: filteredData,
         };
 
-        // res.setHeader(
-        //   "Cache-Control",
-        //   "s-maxage=43200, stale-while-revalidate"
-        // );
+        res.setHeader(
+          "Cache-Control",
+          "s-maxage=43200, stale-while-revalidate"
+        );
 
         res.status(200).json(obj);
       } catch (error) {
