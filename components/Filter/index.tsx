527c518904411c168f1d2c4a1561898607e24b2f
vorsakha <andreitf.dev@gmail.com>
2021-08-30 16:11:26 -0300

    Filter - fix spacing

--- components/Filter/index.tsx
+++ components/Filter/index.tsx
@@ -147,7 +147,7 @@ const Filter = () => {
       </div>
 
       <button
-        className="p-4 whitespace-nowrap text-purple-700"
+        className="p-4 pr-0 whitespace-nowrap text-purple-700"
         onClick={() => setOpen(!open)}
       >
         {open ? (
