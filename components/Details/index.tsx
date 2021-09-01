9362083d2d21712825d36999a90c3b8c05ded550
vorsakha <andreitf.dev@gmail.com>
2021-09-01 08:27:17 -0300

    Details - add issue link

--- components/Details/index.tsx
+++ components/Details/index.tsx
@@ -21,10 +21,22 @@ const Details = ({ data }: DataType) => {
   return (
     <div className="relative text-gray-800 py-4">
       <Alert />
-      <Link href="/">
-        <a className={`py-1`}>{"< Voltar"}</a>
-      </Link>
-      <Fav id={data?.id} className="text-2xl" />
+      <div className="">
+        <div className="">
+          <Link href="/">
+            <a className={`py-1`}>{"< Voltar"}</a>
+          </Link>
+          <a
+            className="ml-4"
+            href={data?.html_url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Issue
+          </a>
+        </div>
+        <Fav id={data?.id} className="text-2xl p-4" />
+      </div>
       <h1 className="mt-4 text-3xl md:text-4xl font-bold">{data?.title}</h1>
       <div className="font-nunito my-4">
         <ReactMarkdown skipHtml={true} remarkPlugins={[remarkGfm]}>
