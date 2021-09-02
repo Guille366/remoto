471e47253c1574340706390a5d4a0e8eb638d83a
vorsakha <andreitf.dev@gmail.com>
2021-09-02 08:10:13 -0300

    Jobs - new bagde color updt

--- components/Jobs/index.tsx
+++ components/Jobs/index.tsx
@@ -60,7 +60,7 @@ const Jobs = () => {
 
               <Link href={`/jobs/${item.id}`} prefetch={false}>
                 <a className="text-gray-700 flex flex-col justify-center h-full p-4 no-underline shadow-md rounded border-purple-700 border border-opacity-25 hover:shadow-lg hover:border-opacity-50">
-                  <small className="text-red-700 opacity-80 font-bold font-code absolute top-0 left-0 p-2 pt-1">
+                  <small className="text-red-600 opacity-80 font-bold font-code absolute top-0 left-0 p-2 pt-1">
                     {formatDate(item.created_at) === formatDate(today) &&
                       "Nova!"}
                   </small>
