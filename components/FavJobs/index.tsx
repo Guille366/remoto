94bac1a3b0a7d2b7e339caf4bbfaee4aa156fd6d
vorsakha <andreitf.dev@gmail.com>
2021-10-26 13:51:23 -0300

    add checks for undefined data

--- components/FavJobs/index.tsx
+++ components/FavJobs/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { dateFormatter, tagFormatter } from "../../utils/formatters";
 import Alert from "../common/Alert";
 import Fav from "../common/Fav";
+import LoadingSpinner from "../common/LoadingSpinner";
 import JobsAvailable from "../JobsAvailable";
 
 const FavJobs = ({ favData }: FavTypes) => {
@@ -13,40 +14,44 @@ const FavJobs = ({ favData }: FavTypes) => {
       <JobsAvailable totalAvailable={totalAvailable} fav />
 
       <div className="grid md:grid-cols-2 gap-4 mt-4">
-        {favData.length === 0 ? (
-          <h2 className="w-full col-span-2 font-light text-center pt-12 text-xl">
-            Sem favoritos por enquanto...
-          </h2>
-        ) : (
-          favData.map((item) => (
-            <div key={item.id} className="relative">
-              <Fav id={item.id} />
+        {favData !== null && favData !== undefined ? (
+          favData.length === 0 ? (
+            <h2 className="w-full col-span-2 font-light text-center pt-12 text-xl">
+              Sem favoritos por enquanto...
+            </h2>
+          ) : (
+            favData.map((item) => (
+              <div key={item.id} className="relative">
+                <Fav id={item.id} />
 
-              <Link
-                href={{
-                  pathname: `/jobs/[id]`,
-                  query: { id: item.id },
-                }}
-              >
-                <a className="text-gray-700 flex flex-col justify-center h-full p-4 no-underline shadow-md rounded border-red-700 border border-opacity-25 hover:shadow-lg hover:border-opacity-50">
-                  <h2 className="font-code pt-0">{item.title}</h2>
-                  <div className="flex flex-row flex-wrap">
-                    {item.labels.map((item, key) => (
-                      <div
-                        className="font-bold text-sm py-0.5 px-1.5 mx-1 my-1 rounded-lg border border-red-700 text-red-700"
-                        key={key}
-                      >
-                        {tagFormatter(item.name)}
-                      </div>
-                    ))}
-                  </div>
-                  <p className="text-gray-500 text-sm p-0 font-mono">
-                    {dateFormatter(item.created_at)}
-                  </p>
-                </a>
-              </Link>
-            </div>
-          ))
+                <Link
+                  href={{
+                    pathname: `/jobs/[id]`,
+                    query: { id: item.id },
+                  }}
+                >
+                  <a className="text-gray-700 flex flex-col justify-center h-full p-4 no-underline shadow-md rounded border-red-700 border border-opacity-25 hover:shadow-lg hover:border-opacity-50">
+                    <h2 className="font-code pt-0">{item.title}</h2>
+                    <div className="flex flex-row flex-wrap">
+                      {item.labels.map((item, key) => (
+                        <div
+                          className="font-bold text-sm py-0.5 px-1.5 mx-1 my-1 rounded-lg border border-red-700 text-red-700"
+                          key={key}
+                        >
+                          {tagFormatter(item.name)}
+                        </div>
+                      ))}
+                    </div>
+                    <p className="text-gray-500 text-sm p-0 font-mono">
+                      {dateFormatter(item.created_at)}
+                    </p>
+                  </a>
+                </Link>
+              </div>
+            ))
+          )
+        ) : (
+          <LoadingSpinner />
         )}
       </div>
     </div>
--- components/Jobs/index.tsx
+++ components/Jobs/index.tsx
@@ -39,44 +39,48 @@ const Jobs = () => {
         <Filter />
       </div>
       <div className="grid md:grid-cols-2 gap-4 mt-4">
-        {limitedJobsPerPage.length === 0 ? (
-          <LoadingSpinner />
-        ) : (
-          limitedJobsPerPage.map((item) => (
-            <div key={item.id} className="relative">
-              <Fav id={item.id} />
+        {limitedJobsPerPage !== null && limitedJobsPerPage !== undefined ? (
+          limitedJobsPerPage.length === 0 ? (
+            <LoadingSpinner />
+          ) : (
+            limitedJobsPerPage.map((item) => (
+              <div key={item.id} className="relative">
+                <Fav id={item.id} />
 
-              <Link
-                href={{
-                  pathname: `/jobs/[id]`,
-                  query: { id: item.id },
-                }}
-              >
-                <a className="text-gray-700 flex flex-col justify-center h-full p-4 no-underline shadow-md rounded border-purple-700 border border-opacity-25 hover:shadow-lg hover:border-opacity-50">
-                  <small className="text-red-600 opacity-80 font-bold font-code absolute top-0 left-0 p-2 pt-1">
-                    {dateFormatter(item.created_at) === dateFormatter(today) &&
-                      "Nova!"}
-                  </small>
-                  <h2 className="font-code pt-0">
-                    {titleFormatter(item.title)}
-                  </h2>
-                  <div className="flex flex-row flex-wrap">
-                    {item.labels.map((item, key) => (
-                      <div
-                        className="font-bold text-sm py-0.5 px-1.5 mx-1 my-1 rounded-lg border border-purple-700 text-purple-700"
-                        key={key}
-                      >
-                        {tagFormatter(item.name)}
-                      </div>
-                    ))}
-                  </div>
-                  <p className="text-gray-500 text-sm p-0 font-mono">
-                    {dateFormatter(item.created_at)}
-                  </p>
-                </a>
-              </Link>
-            </div>
-          ))
+                <Link
+                  href={{
+                    pathname: `/jobs/[id]`,
+                    query: { id: item.id },
+                  }}
+                >
+                  <a className="text-gray-700 flex flex-col justify-center h-full p-4 no-underline shadow-md rounded border-purple-700 border border-opacity-25 hover:shadow-lg hover:border-opacity-50">
+                    <small className="text-red-600 opacity-80 font-bold font-code absolute top-0 left-0 p-2 pt-1">
+                      {dateFormatter(item.created_at) ===
+                        dateFormatter(today) && "Nova!"}
+                    </small>
+                    <h2 className="font-code pt-0">
+                      {titleFormatter(item.title)}
+                    </h2>
+                    <div className="flex flex-row flex-wrap">
+                      {item.labels.map((item, key) => (
+                        <div
+                          className="font-bold text-sm py-0.5 px-1.5 mx-1 my-1 rounded-lg border border-purple-700 text-purple-700"
+                          key={key}
+                        >
+                          {tagFormatter(item.name)}
+                        </div>
+                      ))}
+                    </div>
+                    <p className="text-gray-500 text-sm p-0 font-mono">
+                      {dateFormatter(item.created_at)}
+                    </p>
+                  </a>
+                </Link>
+              </div>
+            ))
+          )
+        ) : (
+          <LoadingSpinner />
         )}
       </div>
 
