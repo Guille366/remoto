2b51d1dd208cc262ccf943fd32dacdfbbdedd562
vorsakha <andreitf.dev@gmail.com>
2021-10-27 14:53:44 -0300

    implement new icons on pages

--- components/Jobs/index.tsx
+++ components/Jobs/index.tsx
@@ -14,6 +14,7 @@ import {
   tagFormatter,
   titleFormatter,
 } from "../../utils/formatters";
+import getIcon from "../../utils/icons";
 
 const Jobs = () => {
   const context = useContext(JobsContext);
@@ -64,9 +65,10 @@ const Jobs = () => {
                     <div className="flex flex-row flex-wrap">
                       {item.labels.map((item, key) => (
                         <div
-                          className="font-bold text-sm py-0.5 px-1.5 mx-1 my-1 rounded-lg border border-purple-700 text-purple-700"
+                          className="font-bold text-sm flex items-center justify-center py-0.5 px-1.5 mx-1 my-1 rounded-lg border border-purple-700 text-purple-700"
                           key={key}
                         >
+                          {getIcon(tagFormatter(item.name) || "")}{" "}
                           {tagFormatter(item.name)}
                         </div>
                       ))}
