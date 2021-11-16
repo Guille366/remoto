572cf9947e9c823c833794ab759ba87a625da167
vorsakha <andreitf.dev@gmail.com>
2021-11-16 14:27:18 -0300

    update Banner

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import SearchBar from "../SearchBar";
+import heroImage from "../../assets/images/remoto-hero.jpg";
+import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div>
-      <SearchBar />
+    <div className="md:h-96 h-56 w-full flex flex-col items-center justify-center">
+      <div className="absolute md:h-96 h-56 left-0 w-full overflow-hidden -z-1">
+        <Image
+          className="rounded-none"
+          src={heroImage}
+          alt="hero"
+          layout="responsive"
+        />
+      </div>
+      <div>
+        <h1 className="text-white text-xl md:text-3xl text-center md:mb-4 font-bold">
+          Vagas dev para trabalhar de onde quiser.
+        </h1>
+        <SearchBar />
+      </div>
     </div>
   );
 };
