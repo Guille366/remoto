c039c41957b1f553192bd5d2cae3194a37eb8700
vorsakha <andreitf.dev@gmail.com>
2021-11-26 10:03:32 -0300

    add overlay above image

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -7,8 +7,9 @@ const Banner = () => {
   return (
     <div className="md:h-96 h-56 w-full flex flex-col items-center justify-center">
       <div className="absolute md:h-96 h-56 left-0 w-full overflow-hidden -z-1">
+        <div className="absolute md:h-96 left-0 w-full overflow-hidden bg-opacity-30 bg-purple-700" />
         <Image
-          className="rounded-none"
+          className="rounded-none -z-2"
           src={heroImage}
           alt="hero"
           layout="responsive"
@@ -18,7 +19,7 @@ const Banner = () => {
         />
       </div>
       <div>
-        <h1 className="text-white text-xl md:text-3xl text-center md:mb-4 font-bold">
+        <h1 className="text-white text-xl md:text-3xl text-center md:mb-4 font-bold z-10">
           Vagas dev para trabalhar de onde quiser.
         </h1>
         <SearchBar />
