0b328135ef5c90588bdbcb9c7fcdd4b3b7c02d04
vorsakha <andreitf.dev@gmail.com>
2022-03-07 18:05:41 -0300

    fix: bug where mobile version wound not overflow correctly

--- components/Jobs/JobsListItem.tsx
+++ components/Jobs/JobsListItem.tsx
@@ -55,7 +55,7 @@ const ListItem: React.FC<ListItemTypes> = ({ data: { id, created_at, title, reac
             <h2 className="font-code pb-2 pt-0">
               {titleFormatter(title)}
             </h2>
-            <div className="flex flex-row overflow-x-auto snap-x snap-mandatory scrollbar-thin md:scrollbar-thumb-transparent rounded-scroll md:scrollbar-track-transparent scrollbar-thumb-rounded-md scrollbar-thumb-rounded-full hover:scrollbar-thumb-violet-700 hover:scrollbar-track-violet-100 pb-2">
+            <div className="flex flex-row overflow-x-auto snap-x snap-mandatory scrollbar-thin md:scrollbar-thumb-transparent md:scrollbar-track-transparent scrollbar-thumb-rounded-md scrollbar-thumb-rounded-full hover:scrollbar-thumb-violet-700 hover:scrollbar-track-violet-100 pb-2">
               {labels.map(
                 (item, key) =>
                   <Labels
