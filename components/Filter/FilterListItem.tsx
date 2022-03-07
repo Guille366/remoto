5c17ff32582e97116bb9335631b4f89b2dc771d5
vorsakha <andreitf.dev@gmail.com>
2022-03-07 07:43:21 -0300

    refactor: rework filter UI

---
+++ components/Filter/FilterListItem.tsx
@@ -0,0 +1,33 @@
+import React, { ChangeEvent } from 'react'
+
+interface CheckboxTypes {
+    active?: boolean
+    text: string
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void
+    name: string
+}
+
+const Checkbox: React.FC<CheckboxTypes> = ({ active, text, handleChange, name }) => {
+  return (
+    <div
+        className={`${
+            active ? "sm:text-purple-700" : ""
+        } py-3 flex flex-row items-center`}
+        >
+        <label className="mr-1 cursor-pointer" htmlFor={name}>
+            {text}
+        </label>
+        <input
+            className={`mr-3 cursor-pointer`}
+            type="checkbox"
+            id={name}
+            name={name}
+            checked={active}
+            onChange={handleChange}
+            data-testId={`${name}-input`}
+        />
+    </div>
+  )
+}
+
+export default Checkbox
